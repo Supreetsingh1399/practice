321594c5f139dec845843fd1579b69af56f3d588
supreet <supreetsingh1399@gmail.com>
2024-08-26 08:28:52 +0530

    chore: update main.java with Hello World loop

---
+++ java_folder/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello World!"+i);
+        }
+    }
+}
